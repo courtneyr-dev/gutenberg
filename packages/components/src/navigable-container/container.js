97e70ecc05910662a66239499d341ba1df1d6c74
Marco Ciampini <marco.ciampo@gmail.com>
2023-04-15 14:52:54 +0200

    Move `isTab` prevent default only when the component is actually handling the focus

--- packages/components/src/navigable-container/container.js
+++ packages/components/src/navigable-container/container.js
@@ -102,11 +102,7 @@ class NavigableContainer extends Component {
 			const targetHasMenuItemRole =
 				MENU_ITEM_ROLES.includes( targetRole );
 
-			// `preventDefault()` on tab to avoid having the browser move the focus
-			// after this component has already moved it.
-			const isTab = event.code === 'Tab';
-
-			if ( targetHasMenuItemRole || isTab ) {
+			if ( targetHasMenuItemRole ) {
 				event.preventDefault();
 			}
 		}
@@ -126,9 +122,16 @@ class NavigableContainer extends Component {
 		const nextIndex = cycle
 			? cycleValue( index, focusables.length, offset )
 			: index + offset;
+
 		if ( nextIndex >= 0 && nextIndex < focusables.length ) {
 			focusables[ nextIndex ].focus();
 			onNavigate( nextIndex, focusables[ nextIndex ] );
+
+			// `preventDefault()` on tab to avoid having the browser move the focus
+			// after this component has already moved it.
+			if ( event.code === 'Tab' ) {
+				event.preventDefault();
+			}
 		}
 	}
 
